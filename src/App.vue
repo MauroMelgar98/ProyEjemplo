8bf6421635281641e2b44e413456bd3fbb59ebfd
MauroMelgar98 <maumel98@gmail.com>
2026-07-13 09:43:52 -0400

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+
+<template>
+  <main>
+  <h1>Sistema de Gestion Emprendimientos</h1>
+  <p>
+    Proyecto de Programacion Web II
+  </p>
+  </main>
+</template>
+
+<style scoped>
+main {
+max-width: 900px;
+margin: 40px auto;
+font-family: Arial, sans-serif;
+}
+h1 {
+  margin-bottom: 8px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue' //Crea la aplicacion 
+import App from './App.vue'
+
+createApp(App).mount('#app') //Inserta los elementos html seleccionados
